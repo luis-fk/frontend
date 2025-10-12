532055688f161a38b2f58d99d32b7a2a37343bc2
felipelk <email@email.com>
2025-10-12 16:59:36 -0300

    Layout html tags improvement

--- src/app/political-culture/layout.tsx
+++ src/app/political-culture/layout.tsx
@@ -11,9 +11,5 @@ export default function PoliticalCultureLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
